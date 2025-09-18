ddf7e5e4c849975d7b464f217cf50ea4b691556b
Mac <mac@Mataabs-MacBook-Pro.local>
2025-09-18 14:23:05 -0600

    Lab-03

---
+++ ListyCityLab3/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.listycitylab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements
+        AddCityFragment.AddCityDialogListener {
+    private ArrayList<City> dataList;
+    private ListView cityList;
+    private CityArrayAdapter cityAdapter;
+
+    @Override
+    public void addCity(City city) {
+        if (!dataList.contains(city)){
+            dataList.add(city);
+        }
+        cityAdapter.notifyDataSetChanged();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] cities = {"Edmonton", "Vancouver", "Toronto"};
+        String[] provinces = {"AB", "BC", "ON"};
+        dataList = new ArrayList<>();
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
+        cityList = findViewById(R.id.city_list);
+        cityAdapter = new CityArrayAdapter(this, dataList);
+        cityList.setAdapter(cityAdapter);
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        fab.setOnClickListener(v -> {
+            new AddCityFragment().show(getSupportFragmentManager(), "Add City");
+        });
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            City pickedCity = dataList.get(position);
+            AddCityFragment editCityBox = AddCityFragment.newInstance(pickedCity);
+            editCityBox.show(getSupportFragmentManager(), "Edit City");
+        });
+
+    }
+}
